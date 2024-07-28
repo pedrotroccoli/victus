9156a72cb7c5e4fb7bdb6a705b642f015efa6133
troccoli <pedroaugustobelzkai@gmail.com>
2024-07-28 15:18:24 -0300

    feat: first commit

---
+++ src/components/header/index.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "../ui/button";
+
+export const Header = () => {
+  const { loginWithRedirect, isAuthenticated, logout } = useAuth0();
+  return (
+    <header className="shadow-md h-16">
+      <div className="max-w-screen-lg mx-auto h-full flex items-center justify-between px-8">
+        <div></div>
+
+          <div className='flex gap-4 items-center'>
+            <Button className='w-32 h-8' variant='outline'>
+              Crie sua conta
+            </Button>
+
+            <Button className='w-32 h-8 font-xs' onClick={() => loginWithRedirect()}>
+              Acessar
+            </Button>
+
+{isAuthenticated && (
+  <>
+
+<div className='h-6 bg-neutral-400 w-0.5 mx-1'>
+
+</div>
+
+            <Button className='w-16 h-8 font-xs' onClick={() => logout()}>
+              Sair
+            </Button>
+  </>
+)}
+
+
+          </div>
+        </div>
+      </header>
+  );
+};
---
+++ src/providers/auth0.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { Auth0Provider as A0Provider } from "@auth0/auth0-react";
+
+export const Auth0Provider = ({ children }: { children: React.ReactNode }) => {
+  return      (
+  <A0Provider
+  domain="dev-mk8k2aspaux7v0r0.us.auth0.com"
+  clientId="FsWNS57qqRmtLGsHvZmOV7h3LyJdClyE"
+  authorizationParams={{
+    redirect_uri: window.location.origin,
+  }}
+>
+    {children}
+  </A0Provider>
+  );
+}
