e85869f20ea699337ffc0df3dc3f0d92f82f16a4
troccoli <pedroaugustobelzkai@gmail.com>
2025-01-31 11:52:54 -0300

    feat: create <ToggleGroupField /> component in molecules

---
+++ src/components/molecules/form/ToggleGroupField/index.tsx
@@ -0,0 +1,32 @@
+import { ToggleGroup, ToggleGroupProps } from "@/components/ions/toggle-group";
+import { Controller, useFormContext } from "react-hook-form";
+
+interface ToggleGroupFieldProps extends ToggleGroupProps {
+  name: string;
+  options: {
+    label: string;
+    value: string;
+  }[];
+}
+
+export const ToggleGroupField = ({ name, options, ...props }: ToggleGroupFieldProps) => {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field: { onChange, onBlur, value }, fieldState }) => (
+        <ToggleGroup
+          options={options}
+          onValueChange={onChange}
+          onBlur={onBlur}
+          value={value}
+          message={fieldState.error?.message}
+          messageClassName={"text-red-500"}
+          {...props}
+        />
+      )}
+    />
+  )
+}
