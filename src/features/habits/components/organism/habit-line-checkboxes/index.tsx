04f3817d2d9ddfc5c8d4f75a7550bd2e381d2419
troccoli <pedroaugustobelzkai@gmail.com>
2025-02-01 16:28:16 -0300

    feat: update <HabitLineCheboxes /> organism

--- src/features/habits/components/organism/habit-line-checkboxes/index.tsx
+++ src/features/habits/components/organism/habit-line-checkboxes/index.tsx
@@ -1,6 +1,7 @@
 import { cn } from "@/lib/utils";
-import { endOfDay, format, isAfter, isBefore, isSameDay, startOfDay } from "date-fns";
+import { format, getDay, isAfter, isBefore, subDays } from "date-fns";
 import React, { useCallback } from "react";
+import { rrulestr } from "rrule";
 import { HabitCheckbox } from "../../molecules/habit-checkbox";
 
 interface HabitLineCheckboxesProps {
@@ -23,6 +24,48 @@ export function HabitLineCheckboxes({ item, daysInMonth, getHabitCheck, currentD
     onCheckHabit(habit, day);
   }, [onCheckHabit]);
 
+  const isAcceptedByRRule = (habit: Habit, monthDay: string) => {
+    if (habit.recurrence_type === 'daily') return true;
+
+    if (!habit.recurrence_details?.rule) return true;
+
+    const rrule = rrulestr(habit.recurrence_details?.rule);
+    const permittedWeekdays = rrule.options.byweekday;
+    const currentWeekday = getDay(monthDay);
+
+    const isAccepted = permittedWeekdays.includes(currentWeekday);
+
+    return isAccepted;
+  }
+
+  function getHabitRange(habit: Habit, day: string) {
+    const isBeforeHabit = isBefore(day, subDays(habit.start_date, 1));
+    const isAfterHabit = habit.end_date ? isAfter(day, habit.end_date) : false;
+    const isAValidHabitDay = isAcceptedByRRule(habit, day);
+    const isAfterToday = isAfter(day, subDays(currentDay, 1));
+
+    return {
+      isBefore: isBeforeHabit,
+      isAfter: isAfterHabit,
+      isValid: isAValidHabitDay,
+      isAfterToday,
+    };
+  }
+
+  const getType = (item: Habit, day: string) => {
+    const habitRange = getHabitRange(item, day);
+
+    if (habitRange.isBefore || habitRange.isAfter || !habitRange.isValid) return 'blocked';
+
+    if (habitRange.isAfterToday) return 'none';
+
+    const isChecked = getHabitCheck(item, day)?.checked;
+
+    if (isChecked) return 'checked';
+
+    return 'empty';
+  }
+
   return (
     <>
       <div className={
@@ -38,34 +81,26 @@ export function HabitLineCheckboxes({ item, daysInMonth, getHabitCheck, currentD
           <div className={cn("flex")}>
             {daysInMonth.map((monthDay, index) => {
               const formattedDay = format(monthDay, 'MM/dd/yyyy');
-              const isChecked = !!getHabitCheck(item, formattedDay)?.checked;
+              const isToday = format(currentDay, 'MM/dd/yyyy') === formattedDay;
 
-              const habitStartDate = startOfDay(item.start_date);
-              const habitEndDate = endOfDay(item.end_date);
-              const sameDay = isSameDay(monthDay, habitStartDate);
+              const isInTheHabitRange = getHabitRange(item, formattedDay);
+              const type = getType(item, formattedDay);
 
-              const isInfinite = item.recurrence_type === 'infinite';
+              const disabled = !isToday || !isInTheHabitRange.isValid;
 
-              const today = format(currentDay, 'dd/MM/yyyy') === format(monthDay, 'dd/MM/yyyy');
-
-              const isAPastDay = isBefore(monthDay, startOfDay(currentDay));
-
-              const isInTheHabitRange = sameDay || (isAfter(monthDay, habitStartDate) && (isInfinite ? true : isBefore(monthDay, habitEndDate)));
+              console.log(isToday, formattedDay);
 
               return (
                 <HabitCheckbox
                   key={`${item._id}-${formattedDay}`}
+                  disabled={disabled}
                   invertPattern={index % 2 === 0}
+                  type={type}
                   onCheck={handleCheckHabit(item, formattedDay)}
                   item={item}
-                  realDay={monthDay}
-                  isAPastDay={isAPastDay}
-                  today={today}
-                  isChecked={isChecked}
-                  isInTheHabitRange={isInTheHabitRange}
+                  today={isToday}
                   onMouseEnter={() => setIsHovering(true)}
                   onMouseLeave={() => setIsHovering(false)}
-                  range={daysInMonth}
                   isFirst={isFirst}
                   isLast={isLast}
                   className={cn(rowHovering && "border-y-black", rowHovering && index === 0 && "border-l-black", rowHovering && index === daysInMonth.length - 1 && "border-r-black")}
