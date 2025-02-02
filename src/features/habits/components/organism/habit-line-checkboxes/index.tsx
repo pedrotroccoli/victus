38cf7b1f19faa4a103f2c14874c757be88d41c13
troccoli <pedroaugustobelzkai@gmail.com>
2025-02-02 13:14:20 -0300

    feat: improve <HabitLineCheckboxes /> component

--- src/features/habits/components/organism/habit-line-checkboxes/index.tsx
+++ src/features/habits/components/organism/habit-line-checkboxes/index.tsx
@@ -1,10 +1,10 @@
 import { cn } from "@/lib/utils";
-import { format, getDay, isAfter, isBefore, subDays } from "date-fns";
+import { isAcceptedByRRule } from "@/utils/habits";
+import { format, isAfter, isBefore, subDays } from "date-fns";
 import React, { useCallback } from "react";
-import { rrulestr } from "rrule";
 import { HabitCheckbox } from "../../molecules/habit-checkbox";
 
-interface HabitLineCheckboxesProps {
+export interface HabitLineCheckboxesProps {
   item: Habit;
   hideHabits: boolean;
   getHabitCheck: (habit: Habit, day: string) => HabitCheck;
@@ -19,43 +19,41 @@ interface HabitLineCheckboxesProps {
   rowHovering: boolean;
 }
 
+interface HabitRange {
+  isBefore: boolean;
+  isAfter: boolean;
+  isValid: boolean;
+  isAfterToday: boolean;
+  isToday: boolean;
+}
+
+
 export function HabitLineCheckboxes({ item, daysInMonth, getHabitCheck, currentDay, onCheckHabit, isFirst, isLast, before, setIsHovering, rowHovering }: HabitLineCheckboxesProps) {
   const handleCheckHabit = useCallback((habit: Habit, day: string) => () => {
     onCheckHabit(habit, day);
   }, [onCheckHabit]);
 
-  const isAcceptedByRRule = (habit: Habit, monthDay: string) => {
-    if (habit.recurrence_type === 'daily') return true;
 
-    if (!habit.recurrence_details?.rule) return true;
-
-    const rrule = rrulestr(habit.recurrence_details?.rule);
-    const permittedWeekdays = rrule.options.byweekday;
-    const currentWeekday = getDay(monthDay);
-
-    const isAccepted = permittedWeekdays.includes(currentWeekday);
-
-    return isAccepted;
-  }
-
-  function getHabitRange(habit: Habit, day: string) {
+  function getHabitRange(habit: Habit, day: string): HabitRange {
     const isBeforeHabit = isBefore(day, subDays(habit.start_date, 1));
     const isAfterHabit = habit.end_date ? isAfter(day, habit.end_date) : false;
     const isAValidHabitDay = isAcceptedByRRule(habit, day);
     const isAfterToday = isAfter(day, subDays(currentDay, 1));
+    const isToday = format(currentDay, 'MM/dd/yyyy') === day;
 
     return {
       isBefore: isBeforeHabit,
       isAfter: isAfterHabit,
       isValid: isAValidHabitDay,
       isAfterToday,
+      isToday,
     };
   }
 
-  const getType = (item: Habit, day: string) => {
-    const habitRange = getHabitRange(item, day);
+  const getType = (item: Habit, day: string, habitRange: HabitRange) => {
+    if (habitRange.isBefore || habitRange.isAfter) return 'blocked-dark';
 
-    if (habitRange.isBefore || habitRange.isAfter || !habitRange.isValid) return 'blocked';
+    if (!habitRange.isValid) return 'blocked';
 
     if (habitRange.isAfterToday) return 'none';
 
@@ -83,17 +81,13 @@ export function HabitLineCheckboxes({ item, daysInMonth, getHabitCheck, currentD
               const formattedDay = format(monthDay, 'MM/dd/yyyy');
               const isToday = format(currentDay, 'MM/dd/yyyy') === formattedDay;
 
-              const isInTheHabitRange = getHabitRange(item, formattedDay);
-              const type = getType(item, formattedDay);
-
-              const disabled = !isToday || !isInTheHabitRange.isValid;
-
-              console.log(isToday, formattedDay);
+              const habitRange = getHabitRange(item, formattedDay);
+              const type = getType(item, formattedDay, habitRange);
 
               return (
                 <HabitCheckbox
                   key={`${item._id}-${formattedDay}`}
-                  disabled={disabled}
+                  disabled={!habitRange.isToday}
                   invertPattern={index % 2 === 0}
                   type={type}
                   onCheck={handleCheckHabit(item, formattedDay)}
