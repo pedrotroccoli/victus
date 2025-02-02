838404d9a405d3b0f1cd27e68889f2c56816b66d
troccoli <pedroaugustobelzkai@gmail.com>
2025-02-02 13:09:07 -0300

    feat: create utils functions for habits

---
+++ src/utils/habits/index.ts
@@ -0,0 +1,39 @@
+import { getDay } from "date-fns";
+import { rrulestr } from "rrule";
+
+/**
+ * @description Format the weekday to be used in the RRule
+ * The date-fns uses 0 for Sunday, 1 for Monday, etc.
+ * The RRule uses 0 for Monday, 1 for Tuesday, etc.
+ * This function formats the weekday to be used in the RRule
+ * 
+ * @example
+ * formatWeekDay(1) // 0
+ * formatWeekDay(2) // 1
+ * formatWeekDay(3) // 2
+ * formatWeekDay(4) // 3
+ * formatWeekDay(5) // 4
+ * @param weekday - The weekday to format
+ * @returns The formatted weekday
+ */
+
+function formatWeekDay(weekday: number) {
+  const sum = weekday - 1;
+
+  return sum === -1 ? 6 : sum;
+}
+
+export const isAcceptedByRRule = (habit: Habit, monthDay: string) => {
+  if (!habit.recurrence_details?.rule) return true;
+
+  const rrule = rrulestr(habit.recurrence_details?.rule);
+  const permittedWeekdays = rrule.options.byweekday;
+
+  if (!permittedWeekdays) return true;
+
+  const currentWeekday = formatWeekDay(getDay(monthDay));
+
+  const isAccepted = permittedWeekdays.includes(currentWeekday);
+
+  return isAccepted;
+}
