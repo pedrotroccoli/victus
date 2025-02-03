8c4417a206a22bceef9b34fde9a22150f4516419
troccoli <pedroaugustobelzkai@gmail.com>
2025-02-03 14:05:33 -0300

    feat: add responsive design to <BoxesExplanation />

--- src/features/habits/components/atoms/boxes-explanation/index.tsx
+++ src/features/habits/components/atoms/boxes-explanation/index.tsx
@@ -2,10 +2,10 @@ import { HabitBox } from "../../ions/habit-box"
 
 export const BoxesExplanation = () => {
   return (
-    <div className="flex items-center justify-between w-full">
+    <div className="flex items-center justify-between w-full flex-wrap">
       <h1 className="font-[Recursive] text-lg font-medium">Informações</h1>
 
-      <ul className="flex items-center gap-4 mr-4">
+      <ul className="flex items-center gap-4 before-lg:mr-4 flex-wrap mt-4 before-lg:mt-0">
         <li className="flex items-center gap-2">
           <HabitBox type="checked" className="w-6 h-6" />
           <p className="text-xs text-neutral-500">Completado</p>
