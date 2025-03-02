ba58f978c2d507ee66396c4c4e21f37560de7092
troccoli <pedroaugustobelzkai@gmail.com>
2025-03-01 22:57:49 -0300

    refactor: update HabitEmptyBox to support category-based droppable areas

--- src/features/habits/components/molecules/habit-empty-box/index.tsx
+++ src/features/habits/components/molecules/habit-empty-box/index.tsx
@@ -1,17 +1,23 @@
 import { useDroppable } from "@dnd-kit/core";
 
-export const HabitEmptyBox = ({ id }: { id: string }) => {
+interface HabitEmptyBoxProps {
+  category?: HabitCategory;
+}
+
+export const HabitEmptyBox = ({ category }: HabitEmptyBoxProps) => {
+
   const { setNodeRef } = useDroppable({
-    id,
+    id: category?._id || '',
     data: {
       type: 'empty-box',
+      category
     }
   });
 
   return (
     <div ref={setNodeRef}>
       <div className="flex items-center justify-center w-full h-full border border-dashed border-neutral-500 rounded-md p-2 hover:border-black hover:border-solid transition-colors duration-200">
-        <p className="text-xs text-neutral-500">Vazio {process.env.NODE_ENV === 'development' && id}</p>
+        <p className="text-xs text-neutral-500">Vazio {process.env.NODE_ENV === 'development' && category?._id || 'Vazio0'}</p>
       </div>
     </div>
   )
