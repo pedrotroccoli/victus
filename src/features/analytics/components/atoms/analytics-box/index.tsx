a2be33f01877a0a4dfd0e906e359698f09148329
troccoli <pedroaugustobelzkai@gmail.com>
2025-04-01 23:08:47 -0300

    fix: update AnalyticsBox component styles for improved visibility

--- src/features/analytics/components/atoms/analytics-box/index.tsx
+++ src/features/analytics/components/atoms/analytics-box/index.tsx
@@ -7,8 +7,8 @@ interface AnalyticsBoxProps {
 
 export const AnalyticsBox = ({ title, value, loading }: AnalyticsBoxProps) => {
   return (
-    <div className=" bg-white border-2 border-neutral-400 p-3 rounded-md sm:p-4">
-      <h6 className="font-[Recursive] text-base font-medium sm:text-lg">{title}</h6>
+    <div className=" bg-white border border-black p-3 rounded-md sm:p-4">
+      <h6 className="font-[Recursive] text-base font-medium sm:text-lg text-black">{title}</h6>
       <div className="mt-2">
         {loading ? (
           <div className="w-3/4 h-4 bg-stone-200 rounded-md animate-pulse border border-stone-400"></div>
