d57e9410fee2cc1acf805c3c1453e995239e6e04
troccoli <pedroaugustobelzkai@gmail.com>
2025-04-01 23:09:20 -0300

    feat: implement plan selection and checkout session creation in FreezedPage component

--- src/pages/_private-layout/freezed/index.tsx
+++ src/pages/_private-layout/freezed/index.tsx
@@ -1,27 +1,48 @@
 import { PlanBox } from "@/components/atoms/plan-box";
+import { useCreateCheckoutSession } from "@/services/checkout";
 import { useGetPlans } from "@/services/plans/hooks";
+import { Snowflake } from "lucide-react";
+import { useState } from "react";
+import { toast } from "sonner";
 
 export default function FreezedPage() {
   const { data: plans } = useGetPlans();
+  const { mutateAsync: createCheckoutSession } = useCreateCheckoutSession();
+  const [isLoading, setIsLoading] = useState('');
 
-  const handlePlanClick = (plan: Plan.Item) => {
-    console.log(plan);
+  const handlePlanClick = async (plan: Plan.Item) => {
+    try {
+      toast.info('Criando seu plano...')
+      setIsLoading(plan.key);
+
+      const checkoutSession = await createCheckoutSession(plan.key);
+
+      toast.success('Redirecionando para o checkout...')
+
+      window.open(checkoutSession.url, '_blank');
+    } catch (error) {
+      toast.error('Erro ao criar seu plano')
+      console.error(error);
+    }
   }
 
   return (
-    <div className="flex flex-col  h-full bg-red-100">
-      <div className="max-w-screen-lg mx-auto bg-blue-100 w-full px-4 sm:px-8 h-full">
+    <div className="flex flex-col  h-full">
+      <div className="max-w-screen-lg mx-auto w-full px-4 sm:px-8 h-full">
         <div className="border border-neutral-300 rounded-md bg-white p-4 mt-8">
 
           <h1 className="text-2xl font-medium font-[Recursive]">Victus Journal</h1>
 
           <div className="w-full border-t border-t-neutral-300 my-8"></div>
 
-          <div className="flex flex-col gap-4">
-            <h2 className="text-lg font-medium font-[Recursive]">
+          <div className="flex flex-col">
+            <div className="flex items-center justify-center w-12 h-12 bg-neutral-100 rounded-full border border-neutral-300">
+              <Snowflake size={32} className="text-blue-500" />
+            </div>
+            <h2 className="text-lg font-medium font-[Recursive] mt-8">
               Sua conta está congelada
             </h2>
-            <p className="text-neutral-500">
+            <p className="text-neutral-500 mt-4">
               Aparentemente você não possui uma assinatura ativa. Por favor, assine um plano para continuar usando o Victus Journal.
             </p>
           </div>
@@ -32,6 +53,8 @@ export default function FreezedPage() {
             {plans?.map((plan, index) => (
               <li key={plan.key}>
                 <PlanBox
+                  loading={isLoading === plan.key}
+                  disabled={!!isLoading}
                   name={plan.name}
                   price={plan.price}
                   features={plan.features}
