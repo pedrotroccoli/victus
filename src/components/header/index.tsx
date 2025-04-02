fb003d4e2ba7f2735d5cc78359d21285f47c6f95
troccoli <pedroaugustobelzkai@gmail.com>
2025-04-02 10:51:24 -0300

    feat: update header component with logo and navigation link to plans

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,17 +1,23 @@
 'use client';
 
+import Image from "next/image";
+import Link from "next/link";
 import { Button } from "../ui/button";
 
 export const Header = () => {
   return (
     <header className="shadow-md h-16">
       <div className="max-w-screen-lg mx-auto h-full flex items-center justify-between px-8">
-        <div></div>
+        <div>
+          <Image src="/logo-with-text.svg" alt="Victus Journal" width={80} height={24} />
+        </div>
 
         <div className='flex gap-4 items-center'>
-          <Button className='w-32 h-8' variant='outline' onClick={() => window.open('https://app.victusjournal.com/sign-up', '_blank')}>
-            Crie sua conta
-          </Button>
+          <Link href="/plans">
+            <Button className='w-32 h-8' variant='outline'>
+              Ver planos
+            </Button>
+          </Link>
 
           <Button className='w-32 h-8 font-xs' onClick={() => window.open('https://app.victusjournal.com/sign-in', '_blank')}>
             Acessar
