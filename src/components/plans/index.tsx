76dbeccda3b0d08e8ab199c6d5d5e13f5b7b3842
troccoli <pedroaugustobelzkai@gmail.com>
2025-04-14 11:57:59 -0300

    fix: update link in plans component from sign-in to sign-up page

--- src/components/plans/index.tsx
+++ src/components/plans/index.tsx
@@ -70,7 +70,7 @@ export const Plans = () => {
                   ))}
                 </ul>
                 <div className="px-6 pb-6">
-                  <a href={`https://app.victusjournal.com/sign-in?key=${item.key}`} target="_blank" rel="noopener noreferrer">
+                  <a href={`https://app.victusjournal.com/sign-up?key=${item.key}`} target="_blank" rel="noopener noreferrer">
                     <Button className="w-full flex items-center justify-between px-6 gap-2 py-3 h-auto rounded-lg">
                       Teste grátis por 14 dias
                       <CaretCircleRight size={18} weight="bold" />
