10abee13a2b32f55ce43a5780cc3207260a28dee
troccoli <pedroaugustobelzkai@gmail.com>
2025-04-16 16:32:32 -0300

    feat: enhance PrivateLayout to manage sign-out state and prevent navigation during logout

--- src/pages/_private-layout/index.tsx
+++ src/pages/_private-layout/index.tsx
@@ -2,14 +2,17 @@ import { Header } from "@/components/organisms/header";
 import { useMe } from "@/services/auth";
 import { signOut } from "@/services/auth/services";
 import { Outlet, useLocation, useNavigate } from "@tanstack/react-router";
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useRef } from "react";
 
 export default function PrivateLayout() {
   const { data: me } = useMe()
   const navigate = useNavigate()
   const location = useLocation()
+  const isLoggingOut = useRef(false);
 
   const handleSignOut = async () => {
+    isLoggingOut.current = true;
+
     await signOut();
 
     navigate({
@@ -38,7 +41,11 @@ export default function PrivateLayout() {
   }, [me, navigate])
 
   useEffect(() => {
+    if (isLoggingOut.current) return;
+
     const withoutSubscriptionPages = ['/account', '/freezed', '/checkout']
+
+    console.log(me, (me?.subscription?.status !== 'active' || !me?.subscription), !withoutSubscriptionPages.some(page => location.pathname.includes(page)));
 
     if (!me) return;
 
