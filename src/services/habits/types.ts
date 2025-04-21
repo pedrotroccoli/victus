5c7d8fe3f5e52f3821b6aeaea00d32e74d4188f6
troccoli <pedroaugustobelzkai@gmail.com>
2025-04-21 14:00:52 -0300

    fix: allow habit delta value to be either number or string for improved flexibility in habit tracking

--- src/services/habits/types.ts
+++ src/services/habits/types.ts
@@ -64,7 +64,7 @@ export interface UpdateHabitCheckRequest {
   habit_check_deltas_attributes: {
     _id?: string;
     habit_delta_id: string;
-    value: number;
+    value: number | string;
     _destroy?: boolean;
   }[];
 }
