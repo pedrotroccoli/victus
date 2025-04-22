06d0084b3b2b7e07383262f11b71afe8e312bddf
troccoli <pedroaugustobelzkai@gmail.com>
2025-04-21 23:50:12 -0300

    feat: add support chat functionality and enhance sidebar with Mr. Habbit hover card

--- src/pages/_private-layout/_sidebar/index.tsx
+++ src/pages/_private-layout/_sidebar/index.tsx
@@ -1,6 +1,6 @@
 import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { cn } from "@/lib/utils"
-import { BookOpenText, ChartLine, Gear } from "@phosphor-icons/react"
+import { BookOpenText, ChartLine, Chat, Gear } from "@phosphor-icons/react"
 import { Link, Outlet, useLocation, useNavigate } from "@tanstack/react-router"
 
 import { LogoWithText } from "@/assets/logo-with-text"
@@ -9,6 +9,11 @@ import { useMe } from "@/services/auth"
 import { signOut } from "@/services/auth/services"
 import { useCallback, useRef } from "react"
 import packageJson from "../../../../package.json"
+
+import MrHabbit from "@/assets/rabbit.png"
+import { HoverCard, HoverCardContent, HoverCardTrigger } from "@/components/ui/hover-card"
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
+import { toast } from "sonner"
 
 export const SidebarLayout = () => {
   const { data: me } = useMe()
@@ -55,6 +60,14 @@ export const SidebarLayout = () => {
       default:
         break;
     }
+  }
+
+  const onClickSupport = () => {
+    const message = encodeURIComponent(`Olá, gostaria de falar com o suporte. Meu nome é ${me?.name} e meu email é ${me?.email}`);
+
+    window.open(`https://wa.me/5591989407261?text=${message}`, '_blank');
+
+    toast.success('Mensagem enviada com sucesso!')
   }
 
   return (
@@ -113,8 +126,43 @@ export const SidebarLayout = () => {
       <main className="w-full h-full bg-red-20">
         <Header account={me} handleSignOut={handleSignOut} goTo={goTo} />
         <div className="max-w-screen-xl mx-auto px-4 sm:px-8 h-[calc(100vh-5rem)] overflow-y-auto">
-          <div className="border-x border-neutral-300 min-h-full  bg-[url('/dashboard-bg.png')] bg-cover bg-repeat">
+          <div className="border-x border-neutral-300 min-h-full  bg-[url('/dashboard-bg.png')] bg-cover bg-repeat relative">
             <Outlet />
+          </div>
+          <div className="absolute bottom-8 right-16">
+            <Popover>
+              <HoverCard openDelay={200} closeDelay={0}>
+                <HoverCardTrigger asChild>
+                  <PopoverTrigger asChild>
+                    <button className="w-16 h-16 bg-white border border-neutral-300 rounded-full flex items-center justify-center shadow-2xl">
+                      <img src={MrHabbit} alt="Mr Habbit" className="w-14 h-14 rounded-full p-1" />
+                    </button>
+                  </PopoverTrigger>
+                </HoverCardTrigger>
+                <HoverCardContent className="bg-black text-white shadow-2xl px-2 py-1 w-auto h-auto text-sm max-w-32 text-center">
+                  Converse com Mr. Habbit
+                </HoverCardContent>
+              </HoverCard>
+              <PopoverContent className="p-0 max-w-[22rem] w-full" side="top" align="end">
+                <ul className="grid divide-y divide-neutral-300 ">
+                  <li >
+                    <button className="w-full flex items-center gap-4 px-4 py-2.5 text-sm font-[Recursive] bg-neutral-100 opacity-50 transition-colors text-left hover:cursor-not-allowed" disabled>
+                      <img src={MrHabbit} alt="Mr Habbit" className="w-6 h-6 rounded-full border border-neutral-300" />
+                      Converse com Mr. Habbit
+                      <span className="text-xs text-yellow-500 bg-yellow-500/20 p-1 rounded-full border border-yellow-500 text-center">Em breve</span>
+                    </button>
+                  </li>
+                  <li >
+                    <button className="w-full flex items-center gap-4 px-4 py-2.5 text-sm font-[Recursive] hover:bg-neutral-100 transition-colors text-left" onClick={onClickSupport}>
+                      <div className="w-6 h-6 rounded-full bg-white border border-neutral-300 flex items-center justify-center">
+                        <Chat size={14} weight="bold" />
+                      </div>
+                      Falar com o suporte
+                    </button>
+                  </li>
+                </ul>
+              </PopoverContent>
+            </Popover>
           </div>
         </div>
       </main>
