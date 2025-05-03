9e8a4466c68309cf5e7e41a66e4a96ff6813d478
troccoli <pedroaugustobelzkai@gmail.com>
2025-05-03 13:59:10 -0300

    feat: wrap main application in MiniKitProvider for enhanced functionality

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createRouter, RouterProvider } from '@tanstack/react-router'
+import { MiniKitProvider } from '@worldcoin/minikit-js/minikit-provider'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import packageJson from '../package.json'
 import { Toaster } from './components/ui/sonner'
 import './globals.css'
 import { routeTree } from './routeTree.gen'
-
 // Import package version
 console.log('v:', packageJson['version']);
 
@@ -28,8 +28,10 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <Toaster position='top-center' richColors />
+      <MiniKitProvider>
+        <RouterProvider router={router} />
+        <Toaster position='top-center' richColors />
+      </MiniKitProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 )
