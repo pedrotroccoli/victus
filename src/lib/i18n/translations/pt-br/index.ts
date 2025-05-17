bda1632296496c9d936938457f191d642fae31da
troccoli <pedroaugustobelzkai@gmail.com>
2025-05-17 12:07:58 -0300

    refactor: remove unused content data and update Portuguese translations to include additional language options

--- src/lib/i18n/translations/pt-br/index.ts
+++ src/lib/i18n/translations/pt-br/index.ts
@@ -1,9 +1,11 @@
+import analytics from './analytics.json';
 import common from './common.json';
 import dashboard from './dashboard.json';
 import habit from './habit.json';
 
-export const translations = {
+export default {
   common: common,
   dashboard: dashboard,
-  habit: habit
+  habit: habit,
+  analytics: analytics
 }
