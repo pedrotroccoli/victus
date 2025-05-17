2f08ad29f03906a5337bc8b9782d5bec4847bfd0
troccoli <pedroaugustobelzkai@gmail.com>
2025-05-17 12:40:02 -0300

    feat: add English, Spanish, and Portuguese translations for authentication, forms, and welcome pages to improve user accessibility

--- src/pages/_private-layout/world-app-welcome/index.tsx
+++ src/pages/_private-layout/world-app-welcome/index.tsx
@@ -11,29 +11,33 @@ import Step1Image from "@/assets/steps/journal.png";
 import { SquareImage } from "@/features/world/components/ions/SquareImage";
 import { StepsBullet } from "@/features/world/components/ions/steps-bullet";
 import { useNavigate } from "@tanstack/react-router";
+import { useTranslation } from "react-i18next";
+import Markdown from "react-markdown";
 export const WorldAppWelcomePage = () => {
+  const { t } = useTranslation('welcome');
+  const { t: tCommon } = useTranslation('common');
   const [step, setStep] = useState(0);
   const navigate = useNavigate();
 
   const stepsInformation = [
     {
-      title: "Olá, bem vindo ao Victus Journal!",
-      description: <p>Seu diário de hábitos, pensamentos e experiências. Controle sua vida!</p>,
+      title: t('welcome.step1.title'),
+      description: t('welcome.step1.description'),
       image: Step1Image,
     },
     {
-      title: "Crie seus hábitos e metas",
-      description: <p>Crie um a um seus hábitos, crie metas e obtenha seu <strong>jornal geral</strong> para preencher.</p>,
+      title: t('welcome.step2.title'),
+      description: t('welcome.step2.description'),
       image: Step2Image,
     },
     {
-      title: "Crie deltas para cada hábito",
-      description: <p>Obtenha dados detalhados sobre seus comportamentos no seu analytics.</p>,
+      title: t('welcome.step3.title'),
+      description: t('welcome.step3.description'),
       image: Step3Image,
     },
     {
-      title: "Veja seu analytics",
-      description: <p>Com o analytics você pode ver sua performance e evolução dos seus objetivos.</p>,
+      title: t('welcome.step4.title'),
+      description: t('welcome.step4.description'),
       image: Step4Image,
     },
   ]
@@ -60,7 +64,9 @@ export const WorldAppWelcomePage = () => {
         <div className="mt-8">
           <h1 className="text-2xl font-[Recursive] font-bold" >{stepsInformation[step].title}</h1>
           <p className=" text-black/75 mt-2">
+          <Markdown>
             {stepsInformation[step].description}
+          </Markdown>
           </p>
         </div>
 
@@ -71,7 +77,7 @@ export const WorldAppWelcomePage = () => {
       <div className="fixed bottom-0 w-full bg-white p-4 pt-6 pb-10 border-t border-neutral-300 rounded-t-3xl shadow-2xl">
         <div className="grid gap-4 w-full">
           <Button className="w-full font-[Recursive] font-bold h-12 flex items-center justify-center gap-4" onClick={nextStep}>
-            <p>Próximo</p>
+            <p>{tCommon('next')}</p>
           </Button>
         </div>
       </div>
