b49f6ca42877831e28676cdb6fcfb0b66a61a524
troccoli <pedroaugustobelzkai@gmail.com>
2025-05-17 12:08:16 -0300

    feat: add language selection feature to account avatar dropdown with internationalization support

--- src/features/account/components/ions/index.tsx
+++ src/features/account/components/ions/index.tsx
@@ -1,8 +1,11 @@
 import { Button } from "@/components/ui/button";
+import Flag from "react-world-flags";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSub, DropdownMenuSubContent, DropdownMenuSubTrigger, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Check } from "lucide-react";
+import { useTranslation } from "react-i18next";
 
 interface AccountAvatarProps {
   shortname: string;
@@ -11,6 +14,14 @@ interface AccountAvatarProps {
 }
 
 export const AccountAvatar = ({ shortname, signOut, goTo }: AccountAvatarProps) => {
+  const { t, i18n } = useTranslation('common');
+
+  const currentLanguage = i18n.resolvedLanguage;
+
+  const changeLanguage = (language: string) => () => {
+    i18n.changeLanguage(language);
+  }
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>
@@ -27,9 +38,31 @@ export const AccountAvatar = ({ shortname, signOut, goTo }: AccountAvatarProps) 
             Dashboard
           </Button>
         </DropdownMenuItem>
+          <DropdownMenuSub >
+            <DropdownMenuSubTrigger>
+              <span>{t('language')}</span>
+            </DropdownMenuSubTrigger>
+            <DropdownMenuSubContent>
+              <DropdownMenuItem onClick={changeLanguage('pt-BR')}>
+                <Flag code="BR" size={16} width={16} />
+                <span>{t('portuguese')}</span>
+                {currentLanguage === 'pt-BR' && <Check size={16} />}
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={changeLanguage('en')}>
+                <Flag code="US" size={16} width={16} />
+                <span>{t('english')}</span>
+                {currentLanguage === 'en' && <Check size={16} />}
+              </DropdownMenuItem>
+              <DropdownMenuItem onClick={changeLanguage('es')}>
+                <Flag code="ES" size={16} width={16} />
+                <span>{t('spanish')}</span>
+                {currentLanguage === 'es' && <Check size={16} />}
+              </DropdownMenuItem>
+            </DropdownMenuSubContent>
+          </DropdownMenuSub>
         <DropdownMenuItem asChild className="h-auto">
           <Button variant="ghost" className="w-full justify-start py-1 cursor-pointer" onClick={signOut}>
-            Sair
+            {t('logout')}
           </Button>
         </DropdownMenuItem>
       </DropdownMenuContent>
