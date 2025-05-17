09988e6669ca7717751300d4ae15a101b55a268b
troccoli <pedroaugustobelzkai@gmail.com>
2025-05-17 12:07:49 -0300

    feat: add Spanish translations for analytics, common, dashboard, and habit components to improve user accessibility

---
+++ src/lib/i18n/translations/es/index.ts
@@ -0,0 +1,11 @@
+import analytics from './analytics.json';
+import common from './common.json';
+import dashboard from './dashboard.json';
+import habit from './habit.json';
+
+export default {
+  common,
+  analytics,
+  habit,
+  dashboard,
+}; 
