7160db67a3d7eae150dbcebf40997b1a2282a70a
troccoli <pedroaugustobelzkai@gmail.com>
2025-05-22 11:57:47 -0300

    refactor: update PlanBox component to use 'title' and 'description' props, and enhance button functionality with customizable text

--- src/components/atoms/plan-box/index.tsx
+++ src/components/atoms/plan-box/index.tsx
@@ -1,23 +1,25 @@
-import { CircleCheck, Loader2 } from "lucide-react";
+import { CircleCheck } from "lucide-react";
 
+import { Button } from "@/components/ions/button";
 import { CircleProgress } from "@/components/ions/circle-progress";
-import { Button } from "@/components/ui/button";
 import { CircleDashed } from "lucide-react";
 
 interface PlanBoxProps {
-  name: string;
+  title: string;
   description?: string;
   price: string;
   features: {
-    name: string;
+    description: string;
   }[];
   progress: number;
   onClick: () => void;
   loading?: boolean;
   disabled?: boolean;
+  buttonText: string;
 }
 
-export const PlanBox = ({ name, price, features, progress, onClick, loading = false, disabled = false }: PlanBoxProps) => {
+export const PlanBox = ({ title, price, features, progress, onClick, loading = false, disabled = false, buttonText }: PlanBoxProps) => {
+
   return (
     <div className="border border-neutral-300 rounded-md p-4 bg-white h-full">
       <div className="w-16 h-16 bg-neutral-100 border border-neutral-300 rounded-full">
@@ -25,23 +27,21 @@ export const PlanBox = ({ name, price, features, progress, onClick, loading = fa
           <CircleDashed size={24} />
         </CircleProgress>
       </div>
-      <h2 className="text-2xl font-medium font-[Recursive] mt-4">{name}</h2>
+      <h2 className="text-2xl font-medium font-[Recursive] mt-4">{title}</h2>
       <h3 className="text-lg font-[Recursive] font-bold mt-4">{price}</h3>
       <ul className="mt-4 grid gap-2">
         {features?.map((feature) => (
           <li className="text-sm text-neutral-600 flex items-center gap-2">
             <CircleCheck size={16} />
-            {feature.name}
+            {feature.description}
           </li>
         ))}
       </ul>
       <div className="w-full border-t border-neutral-300 my-4"></div>
-      <Button className="w-full mt-auto font-[Recursive] font-medium" onClick={onClick} disabled={loading || disabled}>
-        {loading ? (
-          <Loader2 className="w-4 h-4 animate-spin" />
-        ) : (
-          'Teste grátis por 14 dias'
-        )}
+      <Button className="w-full mt-auto font-[Recursive] font-medium" onClick={onClick} disabled={loading || disabled}
+      loading={loading}
+      >
+        {buttonText}
       </Button>
     </div>
   )
