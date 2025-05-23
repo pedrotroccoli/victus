e8b88d2a75085b949a86be8915fc8a61cdae6da7
troccoli <pedroaugustobelzkai@gmail.com>
2025-05-23 16:27:43 -0300

    feat: add secondsToHHMMSS function to convert seconds into HH:MM:SS format for improved time handling

--- src/components/ions/time-selector/index.tsx
+++ src/components/ions/time-selector/index.tsx
@@ -19,6 +19,14 @@ export const HHMMSSToSeconds = (value?: number | string): number => {
   return values.reduce((acc, time, index) => acc + Number(time) * times[values.length - index - 1], 0);
 };
 
+export const secondsToHHMMSS = (value: number): string => {
+  const hours = Math.floor(value / 3600);
+  const minutes = Math.floor((value % 3600) / 60);
+  const seconds = value % 60;
+
+  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
+
 import { TimeMask } from './mask';
 
 export interface TimeProps extends Omit<InputHTMLAttributes<HTMLInputElement>, 'onChange'> {
