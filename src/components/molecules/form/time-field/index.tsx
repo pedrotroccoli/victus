e3ccbb328e0041dcfb80536f5fe1c71a79574434
troccoli <pedroaugustobelzkai@gmail.com>
2025-05-23 16:27:53 -0300

    feat: enhance TimeField component with time input handling and internationalization support for explicit time messages

--- src/components/molecules/form/time-field/index.tsx
+++ src/components/molecules/form/time-field/index.tsx
@@ -1,6 +1,8 @@
 import { TimeInput, TimeInputProps } from "@/components/atoms/time-input";
 import { cn } from "@/lib/utils";
+import { useMemo, useState } from "react";
 import { Controller, useFormContext } from "react-hook-form";
+import { useTranslation } from "react-i18next";
 
 interface TimeFieldProps extends TimeInputProps {
   name: string;
@@ -9,6 +11,27 @@ interface TimeFieldProps extends TimeInputProps {
 
 export const TimeField = ({ name, label, className, ...props }: TimeFieldProps) => {
   const { control } = useFormContext();
+  const { t } = useTranslation('form');
+
+  const [time, setTime] = useState<string>('');
+
+  const message = useMemo(() => {
+    if (time.length === 0 ) return ''
+
+    if (time.length === 5) {
+      const [minutes, seconds] = time.split(':');
+
+      if (Number(minutes) === 0 && Number(seconds) === 0) {
+        return '';
+      }
+
+      return t('time.explicit_time_without_hours', { minutes: parseInt(minutes), seconds: parseInt(seconds) });
+    }
+
+    const [hours, minutes, seconds] = time.split(':');
+
+    return t('time.explicit_time', { hours: parseInt(hours), minutes: parseInt(minutes), seconds: parseInt(seconds) });
+  }, [time]);
 
   return (
     <Controller
@@ -19,11 +42,14 @@ export const TimeField = ({ name, label, className, ...props }: TimeFieldProps) 
           {...props}
           label={label}
           onChangeInSeconds={onChange}
+          onChange={(value) => {
+            setTime(value);
+          }}
           onBlur={onBlur}
           currentValue={value}
           className={cn(className, fieldState.error?.message && "border-red-500")}
-          message={fieldState.error?.message}
-          messageClassName="text-red-500"
+          message={fieldState.error?.message ? fieldState.error.message : message}
+          messageClassName={cn(fieldState.error?.message && "text-red-500")}
         />
       )}
     />
