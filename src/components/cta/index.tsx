700b4ee419491a8e31115cb8fb399e3592b29688
troccoli <pedroaugustobelzkai@gmail.com>
2025-05-26 10:39:35 -0300

    feat: add dynamic translations for CTA section in pt-BR.json and update component to utilize translations

--- src/components/cta/index.tsx
+++ src/components/cta/index.tsx
@@ -1,10 +1,13 @@
 import { CaretCircleRight } from "@phosphor-icons/react/dist/ssr"
+import { useTranslations } from "next-intl"
 import Image from "next/image"
 import Link from "next/link"
 import { Grid } from "../grid"
 import { Button } from "../ui/button"
 
 export const CTA = () => {
+  const t = useTranslations('cta');
+
   return (
     <section>
       <Grid className="grid-container h-full md:pt-40 md:pb-20 pt-10 pb-10">
@@ -13,12 +16,15 @@ export const CTA = () => {
           <div className="blur-block absolute bottom-4 left-1/2 w-20 h-20 z-[1]"></div>
 
           <div className="p-8 md:p-16 md:pl-20 max-w-[40rem] z-10">
-            <h4 className="text-white text-lg sm:text-2xl font-bold">Você está a um passo de ter controle
-              total do seu comportamento!</h4>
-            <p className="text-white/70 mt-4">Dentro da plataforma você tem a oportunidade de saber onde melhorar na sua vida e atingir seus objetivos!</p>
+            <h4 className="text-white text-lg sm:text-2xl font-bold">
+              {t('title')}
+            </h4>
+            <p className="text-white/70 mt-4">
+              {t('description')}
+            </p>
             <Link href="/plans">
               <Button className="mt-12 py-5 w-full md:w-80 h-auto flex items-center justify-between gap-2 px-6 rounded-md bg-victus-orange hover:bg-victus-orange/80">
-                Teste grátis por 14 dias
+                {t('button')}
                 <CaretCircleRight size={18} weight="bold" />
               </Button>
             </Link>
