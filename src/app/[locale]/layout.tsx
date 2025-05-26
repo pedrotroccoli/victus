a6e11f9f5b0f454d1a1ca7621fac10e9abeb5a5a
troccoli <pedroaugustobelzkai@gmail.com>
2025-05-26 10:28:27 -0300

    fix: update HTML lang attribute to use dynamic locale for improved internationalization support

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -31,7 +31,7 @@ export default async function RootLayout({
     }
 
   return (
-    <html lang="en">
+    <html lang={locale}>
       <body className={`${InterFont.variable} ${RecursiveFont.variable}`}>
         <NextIntlClientProvider>
           <Header />
