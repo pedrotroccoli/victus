71b623ec39a23d219220117b0c47a9e04601473f
troccoli <pedroaugustobelzkai@gmail.com>
2026-01-07 17:15:53 -0300

    feat: apply the <DashboardProvider> in routes

--- src/routes/_private/_sidebar/dashboard/index.tsx
+++ src/routes/_private/_sidebar/dashboard/index.tsx
@@ -1,6 +1,7 @@
 import { Home } from '@/pages/_private-layout/_sidebar/dashboard'
+import { DashboardProvider } from '@/pages/_private-layout/_sidebar/dashboard/providers/dashboard-provider'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/_private/_sidebar/dashboard/')({
-  component: Home,
+  component: () => <DashboardProvider><Home /></DashboardProvider>,
 })
