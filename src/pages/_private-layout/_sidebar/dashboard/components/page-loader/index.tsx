65a29704e8c9a4e42fab113948689e37ffbf5421
troccoli <pedroaugustobelzkai@gmail.com>
2026-01-07 17:13:14 -0300

    feat: create <DashboardPageLoder /> for the dashboard page

---
+++ src/pages/_private-layout/_sidebar/dashboard/components/page-loader/index.tsx
@@ -0,0 +1,13 @@
+import { LoaderCircle } from "lucide-react"
+
+export const DashboardPageLoader = () => {
+  return (
+    <div className="flex items-center justify-center h-full flex-col border-neutral-300 border rounded-md p-8 min-h-56">
+      <LoaderCircle
+        size={32}
+        className="animate-spin"
+        strokeWidth={1.75}
+      />
+    </div>
+  );
+}
