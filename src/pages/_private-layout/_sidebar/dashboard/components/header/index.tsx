9761f7b7b699e207a3acbc81265c7a3985383112
troccoli <pedroaugustobelzkai@gmail.com>
2026-01-10 19:13:56 -0300

    feat: update header and remove wrong Modal definitions

--- src/pages/_private-layout/_sidebar/dashboard/components/header/index.tsx
+++ src/pages/_private-layout/_sidebar/dashboard/components/header/index.tsx
@@ -1,25 +1,13 @@
 import { Button } from "@/components/ions/button";
-import { Dialog, DialogTrigger } from "@/components/ui/dialog";
-import { CreateHabitModal } from "@/features/habits/components/templates/create-habit-modal";
 import { PlusCircle } from "lucide-react";
 import { useTranslation } from "react-i18next";
-import { DeltaInfo } from "../..";
-import { CreateHabitModalProps } from "@/features/habits/components/templates/create-habit-modal/types";
 
 interface HeaderProps {
-  onCreateHabit: CreateHabitModalProps["onSave"];
-  habits: Habit[];
-  habitCategories: HabitCategory[];
-  deltaOpen?: DeltaInfo;
-  setDeltaOpen: (deltaOpen: DeltaInfo) => void;
+  onCreateHabit: () => void;
 }
 
 export const DashboardHeader = ({
   onCreateHabit,
-  habits,
-  habitCategories,
-  deltaOpen,
-  setDeltaOpen,
 }: HeaderProps) => {
   const { t: tCommon } = useTranslation("common");
   const { t } = useTranslation("dashboard");
@@ -30,30 +18,13 @@ export const DashboardHeader = ({
         {t("greeting", { name })}
       </h1>
 
-      <Dialog>
-        <DialogTrigger asChild>
-          <Button className="w-full flex gap-4 bg-black rounded-md text-white sm:max-w-40">
-            <PlusCircle size={16} />
-            {tCommon("add")}
-          </Button>
-        </DialogTrigger>
-
-        <CreateHabitModal
-          onSave={onCreateHabit}
-          categories={habitCategories || []}
-          habits={habits || []}
-          newDeltas={deltaOpen?.newDeltas}
-          onCreateDelta={() =>
-            setDeltaOpen({
-              open: true,
-              type: "create",
-              habit: undefined,
-              deltaId: "",
-              newDeltas: [],
-            })
-          }
-        />
-      </Dialog>
+      <Button
+        className="w-full flex gap-4 bg-black rounded-md text-white sm:max-w-40"
+        onClick={onCreateHabit}
+      >
+        <PlusCircle size={16} />
+        {tCommon("add")}
+      </Button>
     </div>
   );
 };
